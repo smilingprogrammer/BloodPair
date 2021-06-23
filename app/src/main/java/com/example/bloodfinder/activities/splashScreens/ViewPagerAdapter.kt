f234dd37128aaa44f59b37cc9646acec7691d8e7
Timi <itstimiking@gmail.com>
2021-06-23 16:52:54 +0100

    Update Added image files and viewpager screen

---
+++ app/src/main/java/com/example/bloodfinder/activities/splashScreens/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.bloodfinder.activities.splashScreens
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ViewPagerAdapter(private var images: List<Int>): RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
