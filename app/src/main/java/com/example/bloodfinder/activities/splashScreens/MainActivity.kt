8bdf9c4fd0b81a9101e0f1af14715d0157f97fc3
itstimiking <itstimiking@gmail.com>
2021-07-08 21:26:28 +0100

    UPDATE: Splash screen shared preference check

--- app/src/main/java/com/example/bloodfinder/activities/splashScreens/MainActivity.kt
+++ app/src/main/java/com/example/bloodfinder/activities/splashScreens/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.example.bloodfinder.activities.splashScreens
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.bloodfinder.R
 import com.example.bloodfinder.activities.auth.LoginActivity
+import java.lang.Boolean.getBoolean
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        moveToLogInScreen();
+
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        val appUseState = sharedPref.getBoolean(getString(R.string.app_used_state), false)
+
+        if(!appUseState){
+            val sharedPref = this?.getPreferences(Context.MODE_PRIVATE) ?: return
+            with(sharedPref.edit()) {
+                putBoolean(getString(R.string.app_used_state), true)
+                apply()
+            }
+            moveToSplashScreens();
+        }else {
+            moveToLoginScreen()
+        }
+
+
     }
 
-    private fun moveToLogInScreen() {
+    private fun moveToSplashScreens() {
         startActivity(Intent(this, SplashScreenSlideActivity::class.java));
+        finish();
+    }
+
+    private fun moveToLoginScreen() {
+        startActivity(Intent(this, LoginActivity::class.java));
         finish();
     }
 }
